ddc2c5ce9e66c369b6545b17ea96bd208fd6d5dc
gabriel_arch <archtech.contato@gmail.com>
2024-08-24 15:54:01 -0300

    feat: implement script to build whitelabel and install dependencies

--- scripts/buildWhitelabels.js
+++ scripts/buildWhitelabels.js
@@ -3,10 +3,10 @@
  */
 const {argv} = require('node:process');
 const {resolve} = require('node:path');
+const {execSync} = require('node:child_process');
 const {readdir, cp} = require('node:fs/promises');
 
-const directoryList = [];
-const BLACK_LIST_DIRECTORIES = [
+const BLACK_LIST_ROOT_DIRECTORIES = [
   '.DS_Store',
   '.git',
   '.vscode',
@@ -14,24 +14,26 @@ const BLACK_LIST_DIRECTORIES = [
   'cspell.json',
   'node_modules',
   'scripts',
+  'vendor',
   'whitelabels',
   'whitelabels-resources',
 
+  // ToDo: Remove
   'ios',
 ];
 
-async function readFolders() {
-  const directories = await readdir('./');
+async function readFolders(path) {
+  const directoryList = [];
+  const directories = await readdir(path);
 
   for await (const dirName of directories) {
     directoryList.push(dirName);
   }
+
+  return directoryList;
 }
 
 async function copyFolder(source, destination) {
-  console.log('source', source);
-  console.log('destination', destination);
-
   await cp(source, destination, {
     recursive: true,
   });
@@ -40,11 +42,16 @@ async function copyFolder(source, destination) {
 console.log('Start build whitelabel...');
 (async function () {
   console.log('Reading folders...');
-  await readFolders();
+
+  const appRootPath = resolve(__dirname, '../');
+
+  const directoryList = await readFolders(appRootPath);
 
   const appDirectories = directoryList.filter(
-    item => !BLACK_LIST_DIRECTORIES.includes(item),
+    item => !BLACK_LIST_ROOT_DIRECTORIES.includes(item),
   );
+
+  console.log('Debug:', appDirectories);
 
   const [whitelabelsArgs] = argv.filter(item => item.includes('--whitelabels'));
 
@@ -56,17 +63,70 @@ console.log('Start build whitelabel...');
   console.log('whitelabelsArgs', whitelabelsArgs);
   const whitelabels = whitelabelsArgs.split('=')[1].split(',');
 
-  console.log('Copying folders...');
-  // for (const whitelabelFolder of whitelabels) {
-  //   for (const dir of appDirectories) {
-  //     const sourcePath = resolve(__dirname, '../', dir);
-  //     const destPath = resolve(
-  //       __dirname,
-  //       `../whitelabels/${whitelabelFolder}`,
-  //       dir,
-  //     );
-  //     await copyFolder(sourcePath, destPath);
-  //   }
-  // }
+  console.log('Start copying folders...');
+  for (const whitelabelFolder of whitelabels) {
+    console.log(`Start build whitelabel: ${whitelabelFolder}`);
+
+    let whitelabelPath = '';
+    for (const dir of appDirectories) {
+      let srcPath = `${appRootPath}/${dir}`;
+      whitelabelPath = `${appRootPath}/whitelabels/${whitelabelFolder}`;
+      let destPath = `${whitelabelPath}/${dir}`;
+
+      if (dir === 'android') {
+        const androidFolder = `${appRootPath}/${dir}`;
+
+        const androidDirectories = await readFolders(androidFolder);
+
+        for (const androidDir of androidDirectories) {
+          const androidDirSrcPath = `${srcPath}/${androidDir}`;
+          const androidDirDestPath = `${destPath}/${androidDir}`;
+
+          if (androidDir === 'app') {
+            const androidAppFolder = `${androidFolder}/${androidDir}`;
+
+            const androidAppDirectories = await readFolders(androidAppFolder);
+
+            for (const androidAppDir of androidAppDirectories) {
+              if (androidAppDir === 'build') {
+                continue;
+              }
+
+              const androidAppDirSrcPath = `${androidDirSrcPath}/${androidAppDir}`;
+              const androidAppDirDestPath = `${androidDirDestPath}/${androidAppDir}`;
+
+              await copyFolder(androidAppDirSrcPath, androidAppDirDestPath);
+            }
+          } else {
+            if (androidDir === '.gradle') {
+              continue;
+            }
+
+            await copyFolder(androidDirSrcPath, androidDirDestPath);
+          }
+        }
+      } else {
+        await copyFolder(srcPath, destPath);
+      }
+    }
+
+    console.log(`Finish build whitelabel: ${whitelabelFolder}`);
+
+    console.log(
+      `Start installing dependencies for whitelabel: ${whitelabelFolder}`,
+    );
+
+    const bufferData = execSync('yarn', {
+      cwd: whitelabelPath,
+      stdio: 'pipe',
+    });
+
+    console.log(bufferData.toString('utf-8'));
+
+    console.log(
+      `Finish installing dependencies for whitelabel: ${whitelabelFolder}`,
+    );
+  }
+
+  console.log('Finish build whitelabel...');
 })();
-console.log('Finish build whitelabel...');
